e3a1e79269a4112bade738c1d9364e3e05e8318c
Stephen A Pohl <sapohl@users.noreply.github.com>
2016-06-27 16:11:38 -0400

    Commit system addon code for outofdate notifications

---
+++ addon/bootstrap.js
@@ -0,0 +1,117 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+
+Cu.import("resource://gre/modules/Preferences.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "Services",
+                                  "resource://gre/modules/Services.jsm");
+XPCOMUtils.defineLazyGetter(this, "gStringBundle", function() {
+  return Services.strings.createBundle("chrome://outofdate-notifications/" +
+                                       "locale/" +
+                                       "outofdate-notifications.properties");
+});
+
+const PREF_UPDATE_URL         = "app.update.url.manual";
+const PREF_UPDATE_DEFAULT_URL = "https://www.mozilla.org/firefox";
+
+let gNotification = null;
+
+function startup() {
+  let wm = Cc["@mozilla.org/appshell/window-mediator;1"]
+             .getService(Ci.nsIWindowMediator);
+
+  // Load into any existing windows
+  let browserWindow = wm.getMostRecentWindow("navigator:browser");
+  loadIntoWindow(browserWindow);
+
+  // Load into any new windows
+  wm.addListener(windowListener);
+}
+
+function showDoorhanger(aWindow) {
+  if (!aWindow || !aWindow.gBrowser) {
+    return;
+  }
+  let message = gStringBundle.GetStringFromName("message");
+  let buttons = [
+    {
+      label:       gStringBundle.GetStringFromName("buttonlabel"),
+      accessKey:   gStringBundle.GetStringFromName("buttonaccesskey"),
+      callback: function () {
+        let url = Preferences.get(PREF_UPDATE_URL, PREF_UPDATE_DEFAULT_URL);
+        aWindow.openUILinkIn(url, "tab");
+      }
+    },
+  ];
+  let box =
+    aWindow.document.getElementById("high-priority-global-notificationbox");
+  if (!box) {
+    return;
+  }
+  gNotification = box.appendNotification(message, "outofdate-notifications",
+                                         "", box.PRIORITY_WARNING_MEDIUM,
+                                         buttons);
+  let closeButton = aWindow.document.getAnonymousElementByAttribute(
+    gNotification, "class", "messageCloseButton close-icon tabbable");
+  closeButton.hidden = true;
+}
+
+function loadIntoWindow(aWindow) {
+  if (!aWindow) {
+    return;
+  }
+  showDoorhanger(aWindow);
+}
+
+function unloadFromWindow(aWindow) {
+  if (!aWindow || !gNotification) {
+    return;
+  }
+  let box =
+    aWindow.document.getElementById("high-priority-global-notificationbox");
+  if (!box) {
+    return;
+  }
+  box.removeNotification(gNotification);
+}
+
+var windowListener = {
+  onOpenWindow: function(aWindow) {
+    // Wait for the window to finish loading
+    let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+                           .getInterface(Ci.nsIDOMWindow);
+    domWindow.addEventListener("load", function() {
+      domWindow.removeEventListener("load", arguments.callee, false);
+      loadIntoWindow(domWindow);
+    }, false);
+  },
+
+  onCloseWindow: function(aWindow) {},
+  onWindowTitleChange: function(aWindow, aTitle) {}
+};
+
+function shutdown(aData, aReason) {
+  // When the application is shutting down we normally don't have to clean
+  // up any UI changes made
+  if (aReason == APP_SHUTDOWN) {
+    return;
+  }
+
+  // Stop listening for new windows
+  Services.wm.removeListener(windowListener);
+
+  // Unload from any existing windows
+  let windows = Services.wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    unloadFromWindow(domWindow);
+  }
+}
+
+function install() {}
+
+function uninstall() {}
